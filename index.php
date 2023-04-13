b1334f25a7992bd56932f24cc542b15b06b66365
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-04-13 16:56:27 +0300

    add logic sections

--- index.php
+++ index.php
@@ -1,55 +1,52 @@
 <?php
-// This is line comment
-/*
-This is block comment
+$num = 10;
+if($num > 10) {
+    echo 'suurem';
+} else if( $num < 10) {
+    echo 'väiksem';
+} else {
+    echo 'võrdne';
+}
 
-*/
-const HELLO_WORLD = 1;
-$num = 1;// int
-$bar = 4.1;//float
-$num = 'hello'; // string
-$num = HELLO_WORLD; // constant 
-$num = true; // boolean
-$num = false;
-$num = [1,2,3,4,5]; //array
-$num = 'world';
-echo 'hello \n $num';
-$num = [
-    'red' => 1,
-    'blue' => 2,
-    'green' => 3
-];
-$num = [
-    'red' => 1,
-    3.2 => 2,
-    3,
-    200 => 4,
-    5,
-    'foo' => 'bar'
-];
-$num = 1;
-$num = 1 + 1;
-$num = 1 - 1;
-$num = 1 * 1;
-$num = 1 / 1;
-$num = 4 % 2;
-$num += 1;
-$num -= 1;
-$num *= 1;
-$num /= 1;
-$num %= 1;
-$num++;
-$num--;
-$num = 2;
-$num**=2;
-$num = true && false;
-$num = true || false;
-$num = 10 > 1;
-$num = 10 == 10;
-$num = '10' == 10;
-$num = '10' === 10;
-$num = '10' !== 10;
-$num = '10' != 10;
-$num = 'hello' . 'world';
-$num .= 'World';
-var_dump($num);
+switch($num) {
+    case 1: 
+        echo 'üks';
+        break;
+    case 2: 
+        echo 'kaks';
+        break;
+    default:
+        echo 'default';
+        break;
+}
+
+for($i=0; $i<10; $i++) {
+    echo "kordus $i\n";
+}
+
+while($num<10) {
+    echo "while loop";
+}
+
+do {
+    echo "do while";
+} while($num<10);
+
+/**
+ * @param string $name Name of person who is to be greeted
+ * @return string
+ */
+function hello(string $name):string {
+    return "hello $name";
+}
+
+$text = hello('Kaspar');
+echo $text;
+
+function recursive($i){
+    if($i>0){
+        recursive($i-1);
+    }
+    echo $i;
+}
+recursive(10);
