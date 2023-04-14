1f85a89030f1029ea4fc7611e45fd0d01f6dd823
Kaspar Martin Suursalu <kasparsu@gmail.com>
2023-04-14 11:40:29 +0300

    add composer autoload and vardumper package

--- public/index.php
+++ public/index.php
@@ -2,19 +2,14 @@
 
 //require_once __DIR__ . '/../src/Router.php';
 
-spl_autoload_register(function ($class){
-
-    $class = substr($class, strlen('App\\'));
-
-    require_once __DIR__ . "/../src/$class.php";
-});
+require __DIR__ . '/../vendor/autoload.php';
 
 use App\Controllers\PublicController as Controller;
 
 $router = new App\Router();
 $dog = new App\Dog();
 $controller = new Controller();
-
+dump($_SERVER);
 // switch($_SERVER['REQUEST_URI']){
 //     case '/':
 //         $name = 'Kaspar';
