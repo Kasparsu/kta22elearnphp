dd78e51f5a4f7a8ef029e9b24bfed31a7dfa1069
kaspar martin suursalu <kasparsu@gmail.com>
2023-04-27 15:56:20 +0300

    add router and controller logic

--- public/index.php
+++ public/index.php
@@ -1,24 +1,22 @@
 <?php
 
-//require_once __DIR__ . '/../src/Router.php';
-
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Controllers\PublicController as Controller;
+require __DIR__ . '/../helpers.php';
+require __DIR__ . '/../routes.php';
 
-$router = new App\Router();
-$dog = new App\Dog();
-$controller = new Controller();
-dump($_SERVER);
-// switch($_SERVER['REQUEST_URI']){
-//     case '/':
-//         $name = 'Kaspar';
-//         require 'views/index.php';
-//         break;
-//     case '/about':
-//         require 'views/about.php';
-//         break;
-//     default:
-//         echo '404';
-//         break;
-// }
+$router = new App\Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if($match){
+    if(is_callable($match['action'])){
+        call_user_func($match['action']);
+    } elseif(is_array($match['action']) && isset($match['action'][0]) && isset($match['action'][1])) {
+        $className = $match['action'][0];
+        $controller = new $className();
+        $method = $match['action'][1];
+        $controller->$method();
+    }
+    
+} else {
+    echo '404';
+}
